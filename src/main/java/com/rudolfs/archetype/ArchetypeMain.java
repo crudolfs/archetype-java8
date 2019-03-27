cbca9d2f89f90b608930551794942125789bad3d
Christiaan Rudolfs <crudolfs@pancompany.com>
2019-03-27 23:12:42 +0100

    initial setup archetype maven java8 project

---
+++ src/main/java/com/rudolfs/archetype/ArchetypeMain.java
@@ -0,0 +1,12 @@
+package com.rudolfs.archetype;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArchetypeMain {
+    private static final Logger LOG = LoggerFactory.getLogger( ArchetypeMain.class );
+
+    public static void main(String[] args) {
+        LOG.info("Hi archetype project for Java 8!");
+    }
+}
